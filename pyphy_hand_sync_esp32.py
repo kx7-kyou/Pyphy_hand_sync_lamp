e063e107ef31c533d4101030d9f298d46bfcbed7
kx7-kyou <pkt8950@naver.com>
2025-01-03 10:08:33 +0900

    Update pyphy_hand_sync_esp32.py

--- pyphy_hand_sync_esp32.py
+++ pyphy_hand_sync_esp32.py
@@ -14,29 +14,30 @@ display = ssd1306.SSD1306_I2C(128, 64, i2c)
 display.fill(0)
 display.show()
 
-# Wi-Fi
-ssid = 'ssid'
-password = 'pw'
+#wifi setting
+ssid = 'test'
+password = 'pkt8950a'
 
-wifi = network.WLAN(network.STA_IF)
-wifi.active(True)
-wifi.connect(ssid, password)
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(False)
+sta_if.active(True)
+sta_if.connect(ssid, password)
 
-while not wifi.isconnected():
-    print('Connecting to WiFi...')
+while not sta_if.isconnected():
+    print('Connecting to WiFi:', ssid+":"+password)
     time.sleep(1)
 
 print('Connected to WiFi')
-print('IP Address:', wifi.ifconfig()[0])
+print('IP Address:', sta_if.ifconfig()[0])
 
-# PWM 핀 설정 (GPIO 2)
+#PWM pin setting
 led = machine.Pin(2, machine.Pin.OUT)
 pwm = machine.PWM(led)
-pwm.freq(1000)  # PWM 주파수 설정
+pwm.freq(1000)
 
-# UDP 서버 소켓 설정
-UDP_IP = wifi.ifconfig()[0]  # ESP32의 IP 주소
-UDP_PORT = 5000  # PC에서 보낸 데이터를 받을 포트
+#UDP socket setting
+UDP_IP = sta_if.ifconfig()[0]  #ip
+UDP_PORT = 1235
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
@@ -64,7 +65,8 @@ while True:
 
     if len(RGB_values) == 3:
         try:
-            r, g, b = [int(value) for value in brightness_values]
+            r, g, b = [int(value) for value in RGB_values]
+            
             
             for i in range(0, 12):
                 np[i] = (r, g, b)
